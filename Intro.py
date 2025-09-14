36616ef8d35ac63b78c45850f3f86aec12389e26
NagaSrinuja2504 <nagasrinuja25@gmail.com>
2025-09-14 14:26:03 +0530

    Self Intro

---
+++ Intro.py
@@ -0,0 +1,7 @@
+Hello sir..I am Naga Srinuja from B.E.Cse-A.I completed my 12th grade in G.D. Matriculation Higher Secondary Schoolwith 83.1%.I chose this course out of my interest.I want to
+become a good softwaare developer.I feel proud to say that I have secured 11th place with a gol medal in 'National Level Handwriting Champioship' when I was in my 6th grade
+and have participated in 'Spelling Bee' competetions and 'English Proficiency'tests and bought laurels to my school.I have also secured medals for it.I am a volleyball player
+and my hobbies are to dance and listen to music.I am very much interested in dance and I like to participate in any cultural events.There are 4 members in my family.
+My father name is Elumalai and he is working as Sub-Inspector of Police in Coimbatore city.My mother name is Kala Rani and she is working in a co-operative bank in pollachi.
+My brother name is Naga Praveen.He is a graduate of BSc.Agriculture in Karunya University and currently he is preparing for banking exams.My best friends are Miracline and
+Shalini.We came to know each other when we were in our 6th grade.
